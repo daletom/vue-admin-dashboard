e9db5ad79ba48c618662883e9a45eb3633af99c7
Tom Dale <tom@imgix.com>
2019-06-04 16:41:50 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
@@ -23,6 +23,7 @@
     color: #2c3e50;
     &.router-link-exact-active {
       color: #42b983;
+      background-color: black;
     }
   }
 }
