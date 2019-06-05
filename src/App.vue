81558b6bf170c01e160bd66bec4d2afb00e78399
Tom Dale <tom@imgix.com>
2019-06-05 15:42:20 -0700

    updating

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,17 @@
 </template>
 
 <style lang="scss">
+@import "@/global-styles/colors.scss";
+@import "@/global-styles/typography.scss";
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: $system-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $white;
+}
+body {
+  background: $dark-blue;
 }
 #nav {
   padding: 30px;
@@ -22,9 +27,15 @@
     font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
-      color: #42b983;
-      background-color: black;
+      color: $white;
     }
   }
+}
+h1 {
+  @include heading-1;
+}
+
+p {
+  @include large-text-bold($purple);
 }
 </style>
