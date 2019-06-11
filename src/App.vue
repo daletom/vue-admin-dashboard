5314a3e592ce738d8f770f634a62cf7b5a62bc5b
Tom Dale <tom@imgix.com>
2019-06-10 22:37:14 -0700

    updates

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,93 @@ h1 {
 p {
   @include large-text-bold($purple);
 }
+
+h4 {
+  margin: 0;
+  line-height: 34px;
+  font-size: 24px;
+  text-align: center;
+  color: #ffffff;
+}
+
+input {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  box-sizing: border-box;
+  border-radius: 4px;
+  height: 60px;
+  width: 100%;
+  font-size: 20px;
+  color: white;
+  padding-left: 20px;
+  margin-top: 20px;
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+button {
+  background: #56ccf2;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  border: none;
+  height: 60px;
+  width: 100%;
+  font-size: 20px;
+  color: white;
+  margin-top: 20px;
+  margin-bottom: 40px;
+}
+
+a {
+  line-height: 25px;
+  font-size: 16px;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.3);
+  text-decoration: none;
+}
+
+//Theme
+.light-background {
+  background-color: $light-gray;
+}
+
+.dark-background {
+  background-color: $dark-blue;
+}
+
+.light-text {
+  color: $white;
+}
+
+.dark-text {
+  color: $black;
+}
+
+.light-link {
+  color: $white;
+}
+
+.dark-link {
+  color: $black;
+}
+
+.light-field {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+.dark-field {
+  background: rgba(198, 208, 235, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.3);
+  }
+}
 </style>
 
 <script>
--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <div @click="toggleDarkMode" class="toggle" :class="{'light-toggle' : !isDarkMode, 'dark-toggle' : isDarkMode}>
-      <div>
-        <img src="@/assets/sun1.svg" alt="sun">
-        <img src="@/assets/moon.svg" alt="moon">
+    <div
+      @click="toggleDarkMode"
+      class="toggle"
+      :class="{'light-toggle' : !isDarkMode, 'dark-toggle' : isDarkMode}"
+    >
+      <div
+        class="toggle-switch"
+        :class="{'toggle-left' : !isDarkMode, 'toggle-right' : isDarkMode}"
+      >
+        <img v-if="!isDarkMode" src="@/assets/sun1.svg" alt="sun">
+        <img v-if="isDarkMode" src="@/assets/moon.svg" alt="moon">
       </div>
     </div>
   </div>
@@ -27,21 +34,40 @@ export default {
 
 <style lang="scss" scoped>
 .toggle {
-    border-radius: 10px;
-    width: 175px;
-    box-sizing: border-box;
-    margin: 50px auto;
+  border-radius: 10px;
+  width: 175px;
+  box-sizing: border-box;
+  margin: 50px auto;
+  display: flex;
 }
 
 .light-toggle {
-background: linear-gradient(346.78deg, #F7FCFC 0%, #FAFCFA 100%);
-border: 1px solid rgba(0, 0, 0, 0.04);
-box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.1);
+  background: linear-gradient(346.78deg, #f7fcfc 0%, #fafcfa 100%);
+  border: 1px solid rgba(0, 0, 0, 0.04);
+  box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.1);
 }
 
 .dark-toggle {
-background: rgba(255, 255, 255, 0.1);
-border: 1px solid rgba(255, 255, 255, 0.1);
-box-shadow: inset 0px 5px 10px rgba(255, 255, 255, 0.1);
+  background: rgba(255, 255, 255, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  box-shadow: inset 0px 5px 10px rgba(255, 255, 255, 0.1);
+}
+
+.toggle-switch {
+  margin: 2px;
+  padding: 15px 35px;
+  border-radius: 8px;
+}
+
+.toggle-left {
+  transform: translateX(0);
+  background: #ffffff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+}
+
+.toggle-right {
+  transform: translateX(90%);
+  background: #1b233f;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
 }
 </style>
