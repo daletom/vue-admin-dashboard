0f024b4d2f4f8679bd51ff3ab92654e626ba54f9
Tom Dale <tom@imgix.com>
2019-06-19 23:15:15 -0700

    add logouts

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as netlifyIdentityWidget from "netlify-identity-widget";
+import GoTrue from "gotrue-js";
+
+// Initialize GoTrue JS for Netlify
+export const auth = new GoTrue({
+  APIUrl: "https://heaterhomes.netlify.com/.netlify/identity",
+  setCookie: true
+})
 
 Vue.config.productionTip = false;
+
+// Initialize Netflify Identity
+netlifyIdentityWidget.init();
 
 new Vue({
   router,
